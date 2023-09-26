292122e701ee30438a58a19cc3751eb9c7985ae5
Adam Frisch <adamgfrisch@gmail.com>
2023-09-26 14:20:03 -0500

    Update test.js
    Junk JS file with random snippet tests.

--- test.js
+++ test.js
@@ -1,1 +1,35 @@
-console.log(sum(range(1, 10)));
+console.log("Hello World");
+
+console.log("The future says:", future());
+
+function future() {
+    return "You'll never have flying cars";
+}
+
+function greet(who) {
+    console.log("Hello " + who);
+}
+greet("Harry");
+console.log("Bye");
+
+console.log(4 * 4);
+
+let caught = 5 * 5;
+console.log(caught);
+
+let one = 1, two = 2;
+console.log(one + two);
+
+let a = "fourteen";
+console.log(a.length);
+
+let mood = "light";
+console.log(mood);
+mood = "dark";
+console.log(mood);
+
+console.log(Math.max(2, 4));
+
+let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " +
+            theNumber * theNumber);
